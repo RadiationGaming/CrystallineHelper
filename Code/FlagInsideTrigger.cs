0aa42f623334f1766d41154bfc206aedff561220
vitellaryjr <43586575+vitellaryjr@users.noreply.github.com>
2023-11-23 15:10:35 -0700

    flag inside trigger

---
+++ Code/FlagInsideTrigger.cs
@@ -0,0 +1,35 @@
+﻿using Celeste;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vitmod {
+    [CustomEntity("vitellary/flaginsidetrigger")]
+    [Tracked(false)]
+    public class FlagInsideTrigger : Trigger {
+        public string flag;
+        public FlagInsideTrigger(EntityData data, Vector2 offset) : base(data, offset) {
+            flag = data.Attr("flag");
+        }
+
+        public override void OnEnter(Player player) {
+            base.OnEnter(player);
+            SceneAs<Level>().Session.SetFlag(flag, true);
+        }
+
+        public override void OnLeave(Player player) {
+            base.OnLeave(player);
+            SceneAs<Level>().Session.SetFlag(flag, false);
+        }
+
+        public override void Removed(Scene scene) {
+            base.Removed(scene);
+            SceneAs<Level>().Session.SetFlag(flag, false);
+        }
+    }
+}
